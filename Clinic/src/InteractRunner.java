6c8601f6a088b084cfda91c93c0a4bc5b3e8e641
pnovakovskiy <raul1510@yandex.ru>
2016-01-19 23:01:48 +0300

    last changes

--- Clinic/src/InteractRunner.java
+++ Clinic/src/InteractRunner.java
@@ -1,8 +1,17 @@
+import java.util.InputMismatchException;
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
 /**
  * Класс, иммитирующий работу клиники
  */
 public class InteractRunner {
+    private Scanner scanner;
+    private Clinic clinic;
 
+    /**
+     * Печатает главное меню
+     */
     private void printMainMenu() {
         System.out.println("Выберите действие:");
         System.out.println("1. Добавить клиента");
@@ -16,9 +25,79 @@ public class InteractRunner {
         System.out.println("9. Выйти из программы");
     }
 
+    /**
+     * Конструктор класса
+     */
     public InteractRunner() {
-        printMainMenu();
+        scanner = new Scanner(System.in);
+        runExec();
     }
+
+    private void runExec() {
+        int userChoice = 0;
+        while (userChoice != 9) {
+            printMainMenu();
+            try {
+                userChoice = scanner.nextInt();
+
+                switch (userChoice) {
+                    case 1:
+                        addNewClient();
+                        break;
+                }
+            }
+            catch (InputMismatchException exc) {
+                System.out.println(exc.getMessage());
+                userChoice = 0;
+            }
+
+        }
+    }
+
+    private void addNewClient() {
+        String id = "";
+        System.out.println("Введите имя клиента:");
+        while (id.equals("")) {
+            try {
+                id = scanner.nextLine();
+            }
+            catch (NoSuchElementException exc) {
+                id = "";
+                System.out.println(exc.getMessage());
+            }
+        }
+        Pet pet  = addNewPet();
+        clinic.addClient(new Client(id, pet));
+        System.out.println("Клиент добавлен.");
+    }
+
+    /**
+     * Создание нового домашнего питомца
+     * @return Pet домашний питомец
+     */
+    private Pet addNewPet() {
+        System.out.println("Какое домашнее животное необходимо добавить? (СОБАКА или КОШКА):");
+        String petKind = scanner.nextLine();
+        while (!petKind.equalsIgnoreCase("собака") && !petKind.equalsIgnoreCase("кошка")) {
+            System.out.println("Неизвестное домашнее животное. Повторите попытку.");
+            petKind = scanner.nextLine();
+        }
+
+        System.out.println("Введите кличку домашнего животного:");
+        String petName = scanner.nextLine();
+
+        Pet pet = null;
+        if (petKind.equalsIgnoreCase("собака")) {
+            pet = new Dog(petName);
+        }
+        else if (petKind.equalsIgnoreCase("кошка")) {
+            pet = new Cat(petName);
+        }
+
+        return pet;
+    }
+
+
 
     public static void main(String[] args) {
         InteractRunner ir = new InteractRunner();
