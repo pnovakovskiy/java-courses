6b2f1de67b77ceb8ca757ad9934209940b09f07a
pnovakovskiy <raul1510@yandex.ru>
2016-03-01 22:45:05 +0300

    some changes

--- lesson7/Calculator/src/test/java/ru/lesson/calculator/InteractRunnerTest.java
+++ lesson7/Calculator/src/test/java/ru/lesson/calculator/InteractRunnerTest.java
@@ -8,10 +8,11 @@ import java.util.Scanner;
 public class InteractRunnerTest {
     @Test
     public void testDoCalculate() {
-        Scanner scanner = new Scanner("-100.2 / 25");
+        String s = "-100.2 / 25\nno\nexit";
+        Scanner scanner = new Scanner(s);
         Calculator calculator = new Calculator();
         InteractRunner interactRunner = new InteractRunner(scanner, calculator);
-        interactRunner.doCalculate(scanner.nextLine());
+        //interactRunner.doCalculate(scanner.nextLine());
         Assert.assertEquals(calculator.getResult(), -4.008, 0);
     }
 }
